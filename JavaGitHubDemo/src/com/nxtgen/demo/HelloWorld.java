1c563b9b0350cd38eda441d7acc21d15d59b43f1
Vinoth Rathinam <vinothrwins@gmail.com>
2022-04-20 20:08:15 +0530

    Code added

--- JavaGitHubDemo/src/com/nxtgen/demo/HelloWorld.java
+++ JavaGitHubDemo/src/com/nxtgen/demo/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		// Code added by Vinoth 
+		System.out.println("Hello everyone");
 
 	}
 
